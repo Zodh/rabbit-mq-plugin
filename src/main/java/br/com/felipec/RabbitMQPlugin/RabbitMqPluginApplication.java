15d84fa236d53d311b4c0e4f7cda1599154f5445
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-26 22:40:16 -0300

    feat: initial commit

---
+++ src/main/java/br/com/felipec/RabbitMQPlugin/RabbitMqPluginApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipec.RabbitMQPlugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqPluginApplication.class, args);
+	}
+
+}
